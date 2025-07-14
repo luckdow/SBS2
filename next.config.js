f9388ea4eb26c006c8e7bda65646c81bd06dbc6a
luckdow <geliyorlardayi@gmail.com>
2025-07-14 22:28:57 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// luckdow/sbs2/SBS2-c249c1601b5731f9f0996ec8eeee315cd133ea41/next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
@@ -7,8 +9,9 @@ const nextConfig = {
   experimental: {
     esmExternals: 'loose'
   },
+  // BU BLOĞU EKLEYİN VEYA GÜNCELLEYİN
   env: {
-    CUSTOM_KEY: 'my-value',
+    NEXT_PUBLIC_Maps_API_KEY: process.env.NEXT_PUBLIC_Maps_API_KEY,
   },
 }
 
