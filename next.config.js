5b7530d1d0b9d3245857cc762570e7acd4e68d2f
luckdow <geliyorlardayi@gmail.com>
2025-07-14 22:51:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-// luckdow/sbs2/SBS2-c249c1601b5731f9f0996ec8eeee315cd133ea41/next.config.js
+// next.config.js
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   experimental: {
-    esmExternals: 'loose'
+    esmExternals: 'loose',
   },
-  // BU BLOĞU EKLEYİN VEYA GÜNCELLEYİN
+  // --- DÜZELTİLMİŞ BÖLÜM ---
   env: {
     NEXT_PUBLIC_Maps_API_KEY: process.env.NEXT_PUBLIC_Maps_API_KEY,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
